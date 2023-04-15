d801bd558668faa5ddceacdfb533981ba66ed181
Gregor Biswanger <gregor.biswanger@web-enliven.de>
2023-04-15 02:24:08 +0200

    feat(extension): Integrate OpenAI API for commit message generation

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,50 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { OpenAIApi, Configuration } from 'openai';
+import { GitExtension } from './git.d';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	let disposable = vscode.commands.registerCommand('extension.generateCommitMessage', async () => {
+		const git = getGitExtension();
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "commitwhisperer" is now active!');
+		if (!git) {
+			vscode.window.showErrorMessage('Git extension not found');
+			return;
+		}
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('commitwhisperer.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from CommitWhisperer!');
-	});
+		const gitAPI = git.getAPI(1);
 
-	context.subscriptions.push(disposable);
+		const selectedRepo = gitAPI.repositories[0];
+		const diff = await selectedRepo.diff(true);
+		
+		const configuration = new Configuration({
+			apiKey: vscode.workspace.getConfiguration('generateCommitMessage').get('openaiApiKey')
+		});
+		const openai = new OpenAIApi(configuration);
+
+		const prompt = `I want you to act as a commit message generator. I will provide you with information about the git diff, and I would like you to generate an appropriate commit message using the conventional commit format. Do not write any explanations or other words, just reply with the commit message with maximal 50 character.`;
+		const customPrompt = vscode.workspace.getConfiguration('generateCommitMessage').get('commitMessagePrompt');
+		const openAIResponse = await openai.createCompletion({
+			model: "text-davinci-003",
+			prompt: prompt + ' ' + customPrompt + '\n\nTask information:' + diff,
+			temperature: 0.7,
+			max_tokens: 256,
+			top_p: 1,
+			frequency_penalty: 0,
+			presence_penalty: 0,
+		});
+
+		await vscode.commands.executeCommand('git.stageAll');
+		await vscode.commands.executeCommand('workbench.view.scm');
+
+		selectedRepo.inputBox.value = openAIResponse.data.choices[0].text?.trim() || 'No commit message generated';
+    });
+
+    context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
+function getGitExtension() {
+    const vscodeGit = vscode.extensions.getExtension<GitExtension>('vscode.git');
+    return vscodeGit?.exports;
+}
+
 export function deactivate() {}
