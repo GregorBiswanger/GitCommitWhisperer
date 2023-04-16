8aa4f9249ff1a0a54c24b2fabd5f437e34ed0d79
Gregor Biswanger <gregor.biswanger@web-enliven.de>
2023-04-16 23:09:48 +0200

    Refactor extension structure

--- src/extension.ts
+++ src/extension.ts
@@ -1,50 +1,66 @@
 import * as vscode from 'vscode';
 import { OpenAIApi, Configuration } from 'openai';
-import { GitExtension } from './git.d';
+import { GitExtension, Repository } from './git.d';
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('extension.generateCommitMessage', async () => {
-		const git = getGitExtension();
+  let disposable = vscode.commands.registerCommand('extension.generateCommitMessage', async () => {
+    const gitRepository = getGitRepository();
+    const gitDiff = await gitRepository?.diff(true);
+    const message = await generateCommitMessage(gitDiff);
+    await openSourceControlView();
+    writeCommitMessageToInputBox(gitRepository, message);
+  });
 
-		if (!git) {
-			vscode.window.showErrorMessage('Git extension not found');
-			return;
-		}
-
-		const gitAPI = git.getAPI(1);
-
-		const selectedRepo = gitAPI.repositories[0];
-		const diff = await selectedRepo.diff(true);
-		
-		const configuration = new Configuration({
-			apiKey: vscode.workspace.getConfiguration('generateCommitMessage').get('openaiApiKey')
-		});
-		const openai = new OpenAIApi(configuration);
-
-		const prompt = `I want you to act as a commit message generator. I will provide you with information about the git diff, and I would like you to generate an appropriate commit message using the conventional commit format. Do not write any explanations or other words, just reply with the commit message with maximal 50 character.`;
-		const customPrompt = vscode.workspace.getConfiguration('generateCommitMessage').get('commitMessagePrompt');
-		const openAIResponse = await openai.createCompletion({
-			model: "text-davinci-003",
-			prompt: prompt + ' ' + customPrompt + '\n\nTask information:' + diff,
-			temperature: 0.7,
-			max_tokens: 256,
-			top_p: 1,
-			frequency_penalty: 0,
-			presence_penalty: 0,
-		});
-
-		await vscode.commands.executeCommand('git.stageAll');
-		await vscode.commands.executeCommand('workbench.view.scm');
-
-		selectedRepo.inputBox.value = openAIResponse.data.choices[0].text?.trim() || 'No commit message generated';
-    });
-
-    context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
-function getGitExtension() {
-    const vscodeGit = vscode.extensions.getExtension<GitExtension>('vscode.git');
-    return vscodeGit?.exports;
+function getGitRepository() {
+  const git = vscode.extensions.getExtension<GitExtension>('vscode.git')?.exports;
+
+  if (!git) {
+    vscode.window.showErrorMessage('Git extension not found');
+    return;
+  }
+
+  return git.getAPI(1).repositories[0];
+}
+
+async function generateCommitMessage(gitDiff: string | undefined) {
+  const configuration = new Configuration({
+    apiKey: vscode.workspace.getConfiguration('generateCommitMessage').get('openaiApiKey'),
+  });
+
+  const openai = new OpenAIApi(configuration);
+
+  const prompt = `I want you to act as a commit message generator. I will provide you with information about the git diff, and I would like you to generate an appropriate commit message using the conventional commit format. Do not write any explanations or other words, just reply with the commit message with maximal 50 character.`;
+  const customPrompt = getCustomPrompt();
+  const openAIResponse = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: prompt + ' ' + customPrompt + '\n\nTask information:' + gitDiff,
+    temperature: 0.6,
+    max_tokens: 256,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+
+  return openAIResponse.data.choices[0].text?.trim();
+}
+
+function getCustomPrompt() {
+  const customPrompt = vscode.workspace.getConfiguration('generateCommitMessage').get('commitMessagePrompt');
+  return customPrompt || '';
+}
+
+async function openSourceControlView() {
+  await vscode.commands.executeCommand('git.stageAll');
+  await vscode.commands.executeCommand('workbench.view.scm');
+}
+
+function writeCommitMessageToInputBox(gitRepository: Repository | undefined, message: string | undefined) {
+  if (gitRepository) {
+    gitRepository.inputBox.value = message || 'No commit message generated';
+  }
 }
 
 export function deactivate() {}
